f7aace643cddbfd1eb453f90fb9d4a229c8cc222
rajattyagipvr <rajat.tyagi@pvrcinemas.com>
2020-07-29 09:15:29 +0530

    updated for org rajattyagipvr jxl build pipelines across repos

--- update-bot.sh
+++ update-bot.sh
@@ -4,4 +4,4 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-jx step create pr chart --name gcr.io/jenkinsxio/builder-machine-learning --name gcr.io/jenkinsxio/builder-machine-learning-gpu --version ${VERSION} --repo https://github.com/jenkins-x/jenkins-x-platform.git
+jx step create pr chart --name 702769831180.dkr.ecr.ap-south-1.amazonaws.com/702769831180/builder-machine-learning --name 702769831180.dkr.ecr.ap-south-1.amazonaws.com/702769831180/builder-machine-learning-gpu --version ${VERSION} --repo https://github.com/rajattyagipvr/jxboot-helmfile-resources.git
